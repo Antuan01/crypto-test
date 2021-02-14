8082a481bf54ce5c38d0dcab462654173ac1bc9b
Antuan01 <joseitox_4@hotmail.com>
2021-02-13 23:31:43 -0400

    initial commit

---
+++ components/Price.vue
@@ -0,0 +1,7 @@
+<template>
+    <p> holaa bebeee </p>
+</template>
+
+<script>
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      <Price />
+      </div>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
